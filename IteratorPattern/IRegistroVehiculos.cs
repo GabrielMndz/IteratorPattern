de712b02277974881ae7d500e38b8c11418abb2e
Gabriel <gabriel.mendez0@hotmail.com>
2019-05-23 19:42:21 -0400

    IteratorPattern

---
+++ IteratorPattern/IRegistroVehiculos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern
+{
+    public interface IRegistroVehiculos
+    {
+        void InsertarVehiculo(string marca, string modelo, double precio);
+        Vehiculo MostrarInformacionVehiculo(int indice);
+        IIteratorVehiculo ObtenerIterator();
+
+    }
+}
